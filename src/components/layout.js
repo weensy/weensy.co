f04035dbecdc5baed3729dc4b4e2592ef2284a75
weensykim <hello@weensy.co>
2022-01-03 15:19:18 +0900

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,60 +1,35 @@
 import React, { Component } from "react";
 import { Link } from "gatsby"
-import { ThemeToggler } from "gatsby-plugin-dark-mode"
 
 class Layout extends Component {
   render() {
     const { title, description, children} = this.props
-    const toggler = (
-      <div className="toggler">
-        <ThemeToggler>{({ theme, toggleTheme }) => (
-          <label className="tog">
-            <input
-              type="checkbox"
-              onChange={e =>
-                toggleTheme(e.target.checked ? "dark" : "light")
-              }
-              checked={theme === "dark"}
-              className="tog-checkbox"
-            />
-            {theme === "dark" ? (
-              <div className="tog-text">
-                Light
-              </div>
-            ) : (
-              <div className="tog-text">
-                Dark
-              </div>
-            )}
-          </label>
-        )}</ThemeToggler>
-      </div>
-    )
 
     return (
       <div className="site-container">
         <div className="header-container">
+          <Link
+            className="header-title"
+            to={`/`}
+          >
+            {title}
+          </Link>
           <div className="nav-container">
-                <Link
-                  className="header-title"
-                  to={`/`}
-                >
-                  {title}
-                </Link>
             <ul className="header-nav">
-              {/* <li style={{marginLeft: "0em"}}> */}
-              <li>
-                <Link to={`/about`}>About</Link>
-              </li>
+              <li><Link to={`/about`}>About</Link></li>
+              <li><Link to={`/works`}>Works</Link></li>
+            </ul>
+            <ul className="header-nav">
+              <li><Link to={`/posts`}>Posts</Link></li>
               <li><Link to={`/tags`}>Tags</Link></li>
-              <li>{toggler}</li>
-              {/* <li><a href="https://aesthetic.codes" target="_blank" rel="noopener noreferrer">Devlog</a></li> */}
+              <li><Link to={`/posts`}>Series</Link></li>
             </ul>
           </div>
         </div>
         <main>{children}</main>
         <footer className="footer-copyright">
-          © {new Date().getFullYear()} {title}, {description}
+          © {new Date().getFullYear()} {title}
+          {/* © {new Date().getFullYear()} {title}, {description} */}
         </footer>
       </div>
     )
