d9a49757aba0a61819110ad455d2cd93e45c549a
weensykim <hello@weensy.co>
2022-01-09 12:47:10 +0900

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,31 +14,57 @@ const AboutPage = ({ data }) => {
     <Layout title={siteTitle} description={siteDescription}>
       <SEO title="About"/>
       <article>
-        <section style={{textAlign: "center"}}>
-        <h1 style={{fontSize: "4.25em"}}>cxsmxs</h1>
-        <h6 style={{marginTop: "0em"}}>: A cosmos I see, hear, feel</h6>
+        <div style={{display:"flex",justifyContent:"center"}}>
+          <div className="status-card">
+            <h4>
+            weensy-kim {'{'}
+            </h4>
+            <p>
+              job: software-engineer;<br/>
+              lives-in: japan;<br/>
+              from: south-korea;<br/>
+              loves: crafting, typing;
+            </p>
+            <h4>
+              {'}'}
+            </h4>
+          </div>
+        </div>
+        <section style={{maxWidth:"40em"}}>
+          <h4 style={{marginTop:"0em"}}>
+            Hello, there! 👋
+          </h4>
+          <p>
+            I'm a developer living in Japan as a foreigner.<br/>
+            I was born and raised in South Korea and have been living in Japan since 2018.<br/>
+            I am writing a blog in en, ko, ja. It's about some dev things, hobbies and daily life.<br/>
+            I'm writing articles for leaving my personal thoughts as a post, rather than to provide information.
+            Therefore, it may be different from the facts or it may contain a strong opinion of mine. Also, I do not monetize my blog.
+          </p>
         </section>
-        <section style={{margin: "7em 0em"}}>
-          <h4>
-            Living as a developer in Japan
-          </h4>
-          <p>
-            This is a blog that writes about Devlog, status, hobbies.<br/>
-            The Devlog is mainly in English, and the rest is mostly written in Korean.<br/>
-            I'm writing articles for leave my personal thoughts as text, rather than for convey information.<br/>
-            Therefore, it is possible that my subjectivity is contained in the article.
-          </p>
-          <h4>
-            일본에서 개발자로 살기
-          </h4>
-          <p>
-            Devlog, 일본생활, 취미 등을 기록하는 블로그입니다.<br/>
-            Devlog는 주로 영어, 나머지는 웬만하면 한국어로 작성합니다.<br/>
-            정보 전달보다는 개인적인 생각을 텍스트로 남기려는 목적으로 기록하고 있습니다.<br/>
-            따라서, 글에 본인의 주관이 담겨 있을 수 있습니다.
-          </p>
-          <p style={{textAlign: "center",marginTop: "5em"}}>
+        <div>
+          <p style={{textAlign: "center",margin: "4em 0em 5em 0em"}}>
             <ul className="social-links">
+            <li><a href="mailto:hello@weensy.co">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="44px" height="44px" viewBox="0 0 44 44" version="1.1">
+                  <defs>
+                    <circle xmlns="http://www.w3.org/2000/svg" id="path" cx="22" cy="22" r="22"/>
+                    <use xmlns="http://www.w3.org/2000/svg" xlinkHref="#path" xmlnsXlink="http://www.w3.org/1999/xlink"/>
+                    <mask xmlns="http://www.w3.org/2000/svg" id="mask" maskContentUnits="userSpaceOnUse" maskUnits="objectBoundingBox" x="0" y="0" width="44" height="44" fill="white">
+                      <use xlinkHref="#path" xmlnsXlink="http://www.w3.org/1999/xlink"/>
+                    </mask>
+                  </defs>
+                  <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <g xmlns="http://www.w3.org/2000/svg" id="github">
+                      <rect id="Rectangle" x="0" y="0" width="44" height="44"/>
+                      <use className="social-icon-use" xmlns="http://www.w3.org/2000/svg" id="Oval" mask="url(#mask)" stroke-width="4" xlinkHref="#path" xmlnsXlink="http://www.w3.org/1999/xlink">
+                        <circle xmlns="http://www.w3.org/2000/svg" id="path" cx="22" cy="22" r="22"/>
+                      </use>
+                      <path className="social-icon-path"d="M13.1115479,18.9319176 C13.389863,19.1136566 14.228726,19.6535732 15.6282877,20.5512492 C17.0278493,21.4489252 18.0999726,22.1401747 18.8448082,22.6248583 C18.9266301,22.6779992 19.1005205,22.7934864 19.3664795,22.9714595 C19.6324384,23.149572 19.8534932,23.293652 20.0293425,23.4034207 C20.2053425,23.5131893 20.4181096,23.6363478 20.6677945,23.7726171 C20.9174795,23.9088864 21.1528493,24.0112628 21.3737534,24.0791882 C21.5948082,24.1475321 21.7994384,24.1814251 21.9876438,24.1814251 L22,24.1814251 L22.0123562,24.1814251 C22.2005616,24.1814251 22.4051918,24.1475321 22.6262466,24.0791882 C22.8471507,24.0112628 23.0826712,23.908747 23.3322055,23.7726171 C23.5817397,23.6362083 23.7945068,23.5131893 23.9705068,23.4034207 C24.1465068,23.293652 24.367411,23.149572 24.6333699,22.9714595 C24.8993288,22.7933469 25.0733699,22.6779992 25.1551918,22.6248583 C25.9080137,22.1400352 27.8233699,20.9090085 30.9006575,18.9316387 C31.4981233,18.545426 31.9971918,18.0794323 32.3983151,17.5339366 C32.799589,16.9887198 33,16.4167233 33,15.8182262 C33,15.3182 32.8054658,14.8901442 32.4166986,14.5340586 C32.0279315,14.177973 31.5674384,14 31.0356712,14 L12.9641781,14 C12.3340137,14 11.8491096,14.196942 11.5094658,14.5908259 C11.1698219,14.9848493 11,15.4773437 11,16.0681695 C11,16.5454609 11.2251233,17.0626427 11.6752192,17.6194362 C12.1253151,18.1762296 12.6041918,18.6137699 13.1115479,18.9319176 Z M31.7722192,20.1477413 C29.0879178,21.8294193 27.0497534,23.1363216 25.6586301,24.0681695 C25.1921096,24.3863173 24.813589,24.6345869 24.5229178,24.8124205 C24.2323973,24.990533 23.8457397,25.1722719 23.3627945,25.3579162 C22.88,25.5435605 22.4299041,25.6363129 22.0125068,25.6363129 L22,25.6363129 L21.9876438,25.6363129 C21.5702466,25.6363129 21.12,25.5435605 20.6372055,25.3579162 C20.154411,25.1722719 19.7676027,24.990533 19.4770822,24.8124205 C19.1865616,24.6345869 18.8080411,24.3863173 18.3415205,24.0681695 C17.2365479,23.3182 15.2027534,22.0112977 12.2399863,20.1477413 C11.7734658,19.8599997 11.360137,19.5302753 11,19.1591262 L11,28.1816343 C11,28.6818 11.192274,29.1097163 11.5769726,29.4658019 C11.9616712,29.822027 12.4241233,30 12.9643288,30 L31.0358219,30 C31.5758767,30 32.0383288,29.822027 32.4230274,29.4658019 C32.8078767,29.1095769 33,28.6818 33,28.1816343 L33,19.1591262 C32.648,19.522604 32.2388904,19.8523284 31.7722192,20.1477413 Z" id="Shape"/>
+                    </g>
+                  </g>
+                </svg>
+              </a></li>
               <li><a href="https://instagram.com/weeensy" target="_blank" rel="noopener noreferrer">
                 <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="44px" height="44px" viewBox="0 0 44 44" version="1.1">
                   <defs>
@@ -59,7 +85,7 @@ const AboutPage = ({ data }) => {
                   </g>
                 </svg>
               </a></li>
-              <li><a href="https://github.com/vaporwavy" target="_blank" rel="noopener noreferrer">
+              <li><a href="https://github.com/weensykim" target="_blank" rel="noopener noreferrer">
                 <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="44px" height="44px" viewBox="0 0 44 44" version="1.1">
                   <defs>
                     <circle xmlns="http://www.w3.org/2000/svg" id="path" cx="22" cy="22" r="22"/>
@@ -79,9 +105,29 @@ const AboutPage = ({ data }) => {
                   </g>
                 </svg>
               </a></li>
+              <li><a href="https://linkedin.com/in/woonjin-kim" target="_blank" rel="noopener noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="44px" height="44px" viewBox="0 0 44 44" version="1.1">
+                  <defs>
+                    <circle xmlns="http://www.w3.org/2000/svg" id="path" cx="22" cy="22" r="22"/>
+                    <use xmlns="http://www.w3.org/2000/svg" xlinkHref="#path" xmlnsXlink="http://www.w3.org/1999/xlink"/>
+                    <mask xmlns="http://www.w3.org/2000/svg" id="mask" maskContentUnits="userSpaceOnUse" maskUnits="objectBoundingBox" x="0" y="0" width="44" height="44" fill="white">
+                      <use xlinkHref="#path" xmlnsXlink="http://www.w3.org/1999/xlink"/>
+                    </mask>
+                  </defs>
+                  <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <g xmlns="http://www.w3.org/2000/svg" id="github">
+                      <rect id="Rectangle" x="0" y="0" width="44" height="44"/>
+                      <use className="social-icon-use" xmlns="http://www.w3.org/2000/svg" id="Oval" mask="url(#mask)" stroke-width="4" xlinkHref="#path" xmlnsXlink="http://www.w3.org/1999/xlink">
+                        <circle xmlns="http://www.w3.org/2000/svg" id="path" cx="22" cy="22" r="22"/>
+                      </use>
+                      <path className="social-icon-path" d="M12.5044685,18.4131131 L16.4026341,18.4131131 L16.4026341,31.9987194 L12.5044685,31.9987194 L12.5044685,18.4131131 Z M14.3541863,16.7137918 L14.3259643,16.7137918 C12.9148636,16.7137918 12,15.6752465 12,14.3600973 C12,13.0180561 12.9419097,12 14.3812324,12 C15.8193791,12 16.7036689,13.0154949 16.7318909,14.3562556 C16.7318909,15.6714048 15.8193791,16.7137918 14.3541863,16.7137918 L14.3541863,16.7137918 Z M32,32 L27.5797272,32 L27.5797272,24.9683698 C27.5797272,23.1281854 26.8882879,21.8732232 25.3678269,21.8732232 C24.2048448,21.8732232 23.5580903,22.7196824 23.2570555,23.537969 C23.1441675,23.8299398 23.1618062,24.2384428 23.1618062,24.6482264 L23.1618062,32 L18.7826905,32 C18.7826905,32 18.8391345,19.5451402 18.7826905,18.4131131 L23.1618062,18.4131131 L23.1618062,20.5452683 C23.420508,19.6130106 24.8198495,18.2824946 27.0529163,18.2824946 C29.8233772,18.2824946 32,20.2379306 32,24.4446152 L32,32 L32,32 Z" id="Shape"/>
+                    </g>
+                  </g>
+                </svg>
+              </a></li>
             </ul>
           </p>
-        </section>
+        </div>
       </article>
     </Layout>
   )
