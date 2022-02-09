e7d05d4f043e095d4da9885b7d95390a9c5e604b
weensykim <hello@weensy.co>
2022-02-10 00:12:50 +0900

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,33 +3,40 @@ import { Link } from "gatsby"
 
 class Layout extends Component {
   render() {
-    const { title, description, children} = this.props
+    const { title, mode, children} = this.props
 
     return (
       <div className="site-container">
-        <div className="header-container">
-          <Link
-            className="header-title"
-            to={`/`}
-          >
-            {title}
-          </Link>
-          <div className="nav-container">
-            <ul className="header-nav">
-              <li><Link to={`/about`}>About</Link></li>
-              <li><Link to={`/works`}>Works</Link></li>
-            </ul>
-            <ul className="header-nav">
-              <li><Link to={`/posts`}>Posts</Link></li>
-              <li><Link to={`/tags`}>Tags</Link></li>
-              <li><Link to={`/series`}>Series</Link></li>
-            </ul>
+        {mode != "footer-only" && 
+          <div className="header-container">
+            <Link
+              className="header-title"
+              to={`/`}
+            >
+              {title}
+            </Link>
+            <div className="nav-container">
+              <ul className="header-nav">
+                {/* <li><Link to={`/about`}>About</Link></li> */}
+                {/* <li><Link to={`/works`}>Works</Link></li> */}
+              </ul>
+              <ul className="header-nav">
+
+                <li><Link to={`/about`}>About</Link></li>
+                <li><Link to={`/works`}>Works</Link></li>
+                <li><Link to={`/posts`}>Blog</Link></li>
+
+                {/* <li><Link to={`/posts`}>Posts</Link></li>
+                <li><Link to={`/tags`}>Tags</Link></li>
+                <li><Link to={`/series`}>Series</Link></li> */}
+              </ul>
+            </div>
           </div>
-        </div>
+        }
         <main>{children}</main>
         <footer className="footer-copyright">
-          © {new Date().getFullYear()} {title}
-          {/* © {new Date().getFullYear()} {title}, {description} */}
+          {/* © {new Date().getFullYear()} {title} */}
+          © 2019 - {new Date().getFullYear()} {title}
         </footer>
       </div>
     )
