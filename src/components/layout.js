549dc2e5a9c56dc22aab206c221f8ef030cdf4f2
weensy <hello@weensy.co>
2022-04-08 23:06:30 +0900

    Fix burger menu in index

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,51 +7,53 @@ const Layout = props => {
   return (
     <div className="site-container">
       {mode !== "footer-only" && 
-          <div className="header-container">
-            <Link
-              className="header-title"
-              to={`/`}
+        <div className="header-container">
+          <Link
+            className="header-title"
+            to={`/`}
+          >
+            {title}
+          </Link>
+          <div className="nav-container">
+            <ul className="header-nav">
+              {/* <li><Link to={`/about`}>About</Link></li> */}
+              {/* <li><Link to={`/works`}>Works</Link></li> */}
+            </ul>
+            <ul className="header-nav">
+
+              <li><Link to={`/about`}>About</Link></li>
+              <li><Link to={`/works`}>Works</Link></li>
+              <li><Link to={`/posts`}>Blog</Link></li>
+
+              {/* <li><Link to={`/posts`}>Posts</Link></li>
+              <li><Link to={`/tags`}>Tags</Link></li>
+              <li><Link to={`/series`}>Series</Link></li> */}
+            </ul>
+          </div>
+          <div className={`header-burger ${toggleNav ? `header-burger-opened` : ``}`}>
+            <div
+              className="hamburger hamburger--collapse"
+              aria-label="Menu"
+              role="button"
+              aria-controls="navigation"
+              onClick={() => setToggleNav(!toggleNav)}
             >
-              {title}
-            </Link>
-            <div className="nav-container">
-              <ul className="header-nav">
-                {/* <li><Link to={`/about`}>About</Link></li> */}
-                {/* <li><Link to={`/works`}>Works</Link></li> */}
-              </ul>
-              <ul className="header-nav">
-
-                <li><Link to={`/about`}>About</Link></li>
-                <li><Link to={`/works`}>Works</Link></li>
-                <li><Link to={`/posts`}>Blog</Link></li>
-
-                {/* <li><Link to={`/posts`}>Posts</Link></li>
-                <li><Link to={`/tags`}>Tags</Link></li>
-                <li><Link to={`/series`}>Series</Link></li> */}
-              </ul>
-            </div>
-            <div className={`header-burger ${toggleNav ? `header-burger-opened` : ``}`}>
-              <div
-                className="hamburger hamburger--collapse"
-                aria-label="Menu"
-                role="button"
-                aria-controls="navigation"
-                onClick={() => setToggleNav(!toggleNav)}
-              >
-                <div className="hamburger-box">
-                  <div className="hamburger-inner" />
-                </div>
+              <div className="hamburger-box">
+                <div className="hamburger-inner" />
               </div>
             </div>
           </div>
+        </div>
       }
-      <div className={`burger-menu ${toggleNav ? `burger-menu-opened` : ``}`}>
-        <ul>
-          <li><Link to={`/about`}>About</Link></li>
-          <li><Link to={`/works`}>Works</Link></li>
-          <li><Link to={`/posts`}>Blog</Link></li>
-        </ul>
-      </div>
+      {mode !== "footer-only" && 
+        <div className={`burger-menu ${toggleNav ? `burger-menu-opened` : ``}`}>
+          <ul>
+            <li><Link to={`/about`}>About</Link></li>
+            <li><Link to={`/works`}>Works</Link></li>
+            <li><Link to={`/posts`}>Blog</Link></li>
+          </ul>
+        </div>
+      }
       <main>{children}</main>
       <footer className="footer-copyright">
         {/* © {new Date().getFullYear()} {title} */}
