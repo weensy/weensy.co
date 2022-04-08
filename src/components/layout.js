c002388201dbbb5db940f4b94310015bd1a5c636
weensy <hello@weensy.co>
2022-04-08 22:52:52 +0900

    Add hamburger menu for mobile

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "gatsby"
 
-class Layout extends Component {
-  render() {
-    const { title, mode, children} = this.props
-
-    return (
-      <div className="site-container">
-        {mode !== "footer-only" && 
+const Layout = props => {
+  const { title, mode, children} = props
+  const [ toggleNav, setToggleNav ] = React.useState(false)
+  return (
+    <div className="site-container">
+      {mode !== "footer-only" && 
           <div className="header-container">
             <Link
               className="header-title"
@@ -31,16 +30,35 @@ class Layout extends Component {
                 <li><Link to={`/series`}>Series</Link></li> */}
               </ul>
             </div>
+            <div className={`header-burger ${toggleNav ? `header-burger-opened` : ``}`}>
+              <div
+                className="hamburger hamburger--collapse"
+                aria-label="Menu"
+                role="button"
+                aria-controls="navigation"
+                onClick={() => setToggleNav(!toggleNav)}
+              >
+                <div className="hamburger-box">
+                  <div className="hamburger-inner" />
+                </div>
+              </div>
+            </div>
           </div>
-        }
-        <main>{children}</main>
-        <footer className="footer-copyright">
-          {/* © {new Date().getFullYear()} {title} */}
-          © 2019 - {new Date().getFullYear()} {title}
-        </footer>
+      }
+      <div className={`burger-menu ${toggleNav ? `burger-menu-opened` : ``}`}>
+        <ul>
+          <li><Link to={`/about`}>About</Link></li>
+          <li><Link to={`/works`}>Works</Link></li>
+          <li><Link to={`/posts`}>Blog</Link></li>
+        </ul>
       </div>
-    )
-  }
+      <main>{children}</main>
+      <footer className="footer-copyright">
+        {/* © {new Date().getFullYear()} {title} */}
+        © 2019 - {new Date().getFullYear()} {title}
+      </footer>
+    </div>
+  )
 }
 
 export default Layout
