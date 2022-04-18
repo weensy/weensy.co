a8c0bc731ea0a881017bfbe3a403c46fa6c5b612
weensy <hello@weensy.dev>
2022-04-18 22:44:26 +0900

    Add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
